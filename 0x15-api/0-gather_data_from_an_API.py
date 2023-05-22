f88b51df2d4f96b35ee5533085b7492d2d5fc2da
brainkipkemboi <bkipkemboi@watuafrica.co.ke>
2023-05-22 20:19:30 +0300

    Frustrations

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,21 +3,36 @@
 import requests
 
 def get_employee_todo_progress(employee_id):
-    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    if response.status_code == 200:
-        employee_data = response.json()
+    # Make a GET request to retrieve the employee data
+    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    employee_response = requests.get(employee_url)
+
+    # Check if the response is successful (status code 200)
+    if employee_response.status_code == 200:
+        employee_data = employee_response.json()
         employee_name = employee_data['name']
-        todos_response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+
+        # Make a GET request to retrieve the TODO list for the employee
+        todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+        todos_response = requests.get(todos_url)
+
+        # Check if the TODO list response is successful (status code 200)
         if todos_response.status_code == 200:
             todos_data = todos_response.json()
             total_tasks = len(todos_data)
             done_tasks = [todo['title'] for todo in todos_data if todo['completed']]
+
+            # Print the employee TODO list progress
             print(f"Employee {employee_name} is done with tasks ({len(done_tasks)}/{total_tasks}):")
             for task_title in done_tasks:
                 print(f"\t{task_title}")
+
         else:
             print(f"Failed to retrieve TODO list for employee ID {employee_id}.")
     else:
         print(f"Failed to retrieve employee data for ID {employee_id}.")
+
+# Test the function with an example employee ID
 employee_id = 1
 get_employee_todo_progress(employee_id)
+
