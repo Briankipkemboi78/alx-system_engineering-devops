369cc831f82097deb2738f438c494a9be5fa70dc
brainkipkemboi <bkipkemboi@watuafrica.co.ke>
2023-05-22 20:29:10 +0300

    Gather Data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Using https://jsonplaceholder.typicode.com
+returns info about employee TODO progress
+Implemented using recursion
+"""
+import re
 import requests
+import sys
 
-def get_employee_todo_progress(employee_id):
-    # Make a GET request to retrieve the employee data
-    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    employee_response = requests.get(employee_url)
 
-    # Check if the response is successful (status code 200)
-    if employee_response.status_code == 200:
-        employee_data = employee_response.json()
-        employee_name = employee_data['name']
+API = "https://jsonplaceholder.typicode.com"
+"""REST API url"""
 
-        # Make a GET request to retrieve the TODO list for the employee
-        todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-        todos_response = requests.get(todos_url)
 
-        # Check if the TODO list response is successful (status code 200)
-        if todos_response.status_code == 200:
-            todos_data = todos_response.json()
-            total_tasks = len(todos_data)
-            done_tasks = [todo['title'] for todo in todos_data if todo['completed']]
-
-            # Print the employee TODO list progress
-            print(f"Employee {employee_name} is done with tasks ({len(done_tasks)}/{total_tasks}):")
-            for task_title in done_tasks:
-                print(f"\t{task_title}")
-
-        else:
-            print(f"Failed to retrieve TODO list for employee ID {employee_id}.")
-    else:
-        print(f"Failed to retrieve employee data for ID {employee_id}.")
-
-# Test the function with an example employee ID
-employee_id = 1
-get_employee_todo_progress(employee_id)
-
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API, id)).json()
+            todos_res = requests.get('{}/todos'.format(API)).json()
+            user_name = user_res.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
